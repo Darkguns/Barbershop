fdd89613c80ecf4fb28b08aff9e1f22d9d248ab4
Darkguns <rds26@mail.ru>
2023-10-03 23:39:19 +0300

    create branch dev-start-project

--- Barbershop/src/main/java/com/example/barbershop/BarbershopApplication.java
+++ Barbershop/src/main/java/com/example/barbershop/BarbershopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BarbershopApplication {
 
 	public static void main(String[] args) {
-		System.out.println("dev");
+		System.out.println("dev-start-project");
 		SpringApplication.run(BarbershopApplication.class, args);
 	}
 
